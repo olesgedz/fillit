e6d96004dad899dfc5202bc6bf637613d1428202
Jeromy Black balaq <jblack-b@e3r7p3.21-school.ru>
2018-12-28 18:21:34 +0300

    gnl

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: olesgedz <olesgedz@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/12/28 01:17:41 by olesgedz          #+#    #+#              #
-#    Updated: 2018/12/28 01:57:27 by olesgedz         ###   ########.fr        #
+#    Updated: 2018/12/28 18:20:15 by jblack-b         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -20,20 +20,17 @@ CC = clang
 CFLAGS= -Wall -Wextra -Werror
 
 LIBFT= ./libft/libft.a
-LIBINC= -I ./libft
+LIBINC= -I ./libft/includes/
 
 all: $(NAME)
+
+$(NAME):
+	@$(CC) $(LIBFT) $(SRC) $(LIBINC)
 
 libft: $(LIBFT)
 
 $(LIBFT):
 	make -C ./libft
-
-$(GNL):
-	clang $(CFLAGS) -o ./get_next_line/get_next_line.c  -I ./get_next_line.h
-
-$(NAME): libft $(OBJ)
-	@$(CC) $(LIBFT) $(GNL) -o $(NAME) $(OBJ)
 
 debug: libft $(OBJ)
 	@$(CC) $(LIBLINK) -g $(SRC)
--- main.c
+++ main.c
@@ -6,25 +6,54 @@
 /*   By: olesgedz <olesgedz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/28 01:24:37 by olesgedz          #+#    #+#             */
-/*   Updated: 2018/12/28 01:50:49 by olesgedz         ###   ########.fr       */
+/*   Updated: 2018/12/28 18:20:39 by jblack-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-#include "libft/includes/libft.h"
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include "get_next_line/get_next_line.h"
+#include <stdio.h>
+#include "libft.h"
+#include "get_next_line.h"
 
-int main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
-	char *line;
-	int fd;
-	argc++;
-	fd = open(argv[1], O_RDONLY);
-	get_next_line(fd, &line);
-	ft_putstr(line);
+	int		fd;
+	int		fd2;
+	char	*line;
 
+
+	if (argc < 2)
+	{
+		ft_putstr("ERROR");
+		return (0);
+	}
+	if (argc > 2)
+	{
+		fd = open(argv[1], O_RDONLY);
+		fd2 = open(argv[2], O_RDONLY);
+		int i = fd;
+		while (get_next_line(i, &line))
+		{
+			ft_putstr(line);
+			free(line);
+			ft_putstr("\n");
+			if (i == fd)
+				i = fd2;
+			else
+				i = fd;
+		}
+	}
+	else
+	{
+		fd = open(argv[1], O_RDONLY);
+		while (get_next_line(fd, &line))
+		{
+			ft_putstr(line);
+			free(line);
+			ft_putstr("\n");
+		}
+	}
 	return (0);
 }
