82ef374ef85d7efc2d78732b189d7952796ff88b
olesgedz <olesgedz@rambler.ru>
2019-01-04 18:05:56 +0300

    `zx

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jblack-b <jblack-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/29 18:10:33 by jblack-b          #+#    #+#             */
-/*   Updated: 2019/01/03 21:03:27 by jblack-b         ###   ########.fr       */
+/*   Updated: 2019/01/04 18:05:05 by jblack-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,22 +36,40 @@ int		ft_validate(t_etris *figure)
 	char **temp;
 	int j;
 	int k;
+	int count;
+	int map;
 
+	map = 1;
 	j = 0;
 	k = 0;
+	count = 0;
 	temp = figure->value;
 	while (j < 4)
 	{
-		if (ft_strchr(figure->value[j], '#'))
+		while (k < 4)
 		{
-			//ft_putstr("\n");
-			//ft_putstr(figure->value[j]);
-			ft_putnbr(j);
-			ft_putstr("\n");
+			if (temp[j][k] != '#' && temp[j][k] != '.')
+				map = 0;
+			if (temp[j][k] == '#')
+			{
+				count++;
+				if (k - 1 >= 0 && temp[j][k - 1] == '#')
+					count++;
+				if (k + 1 <= 3 && temp[j][k + 1] == '#')
+					count++;
+				if (j - 1 >= 0 && temp[j - 1][k] == '#')
+					count++;
+				if (j + 1 <= 3 && temp[j + 1][k] == '#')
+					count++;
+			}
+			k++;
 		}
+		k = 0;
 		j++;
 	}
-	return (0);
+	if ((count == 10 || count == 12) && map == 1)
+		temp->valid = 1;
+	return (temp->valid);
 }
 
 int		main(int argc, char **argv)
@@ -99,7 +117,7 @@ int		main(int argc, char **argv)
 	while (i < 4)
 	{
 		ft_printmap(figures[i]->value);
-		ft_validate(figures[i]);
+		printf("%d\n", ft_validate(figures[i]));
 		i++;
 	}
 	return (0);
